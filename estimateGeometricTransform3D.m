71e8229b80251b734c70fe2d764be2a12c54bee5
etaghizadeh <e.taghizade@gmail.com>
2016-10-21 12:54:38 +0200

    Update estimateGeometricTransform3D.m

--- estimateGeometricTransform3D.m
+++ estimateGeometricTransform3D.m
@@ -1,20 +1,18 @@
-function [T, varargout] = estimateGeometricTransform3D(fix, mov, varargin)
+function [inliers, varargout] = estimateGeometricTransform3D(fix, mov, varargin)
 % [T, inliers] = estimateGeometricTransform3D(fix, mov, options)
 % fix: fix landmarks  Mx3
 % mov: moving landmarks  Mx3 (in correspondence with fix)
 % options:
-%         .numIterations:  number of iterations for Ransac (default 100)
+%         .numIterations:  number of iterations for Ransac (default 1000)
 %         .thr          :  threshold of fitting (default 5)
-%         .d            :  minimum number of points for affin transformation(default 3)
+%         .d            :  minimum number of points for affin transformation(default 4)
 %         .reflection   : 'best', 0, 1 => find the optimum based on 
 
-% [fix, T_fix] = normalizeControlPoints(fix);
-% [mov, T_mov] = normalizeControlPoints(mov);
-
+% check the inputs
 if (nargin == 2)
     options.numIterations = 1000;
-	options.thr           = 5;
-	options.d             = 4;
+	options.thr       = 5;
+	options.d         = 4;
     options.reflection    = 'best';
 else
     if (nargin == 3)
@@ -55,6 +53,8 @@ for ii = 1:options.numIterations
     AffineT    = findAffine(samplesFix, samplesMov);
     movRigid   = [mov ones(size(mov, 1), 1)] * AffineT;
     movRigid   = movRigid(:, 1:3);
+    % if you are interested to find the inliers based on rigid transformation, 
+    %then uncomment the following lines and comment the above three lines.
 %     [~, ~, Transformation] = procrustes(samplesFix, samplesMov, 'reflection', options.reflection);
 %     movRigid = bsxfun(@plus, Transformation.b *  mov * Transformation.T, Transformation.c(1, :));
     X = fix - movRigid;
@@ -64,46 +64,20 @@ for ii = 1:options.numIterations
         numInliers = numel(ind);
         Inliers    = ind;
         distMin = mean(dist);
-        if numInliers == size(fix, 1)
+        if numInliers == size(fix, 1) %if all points are inliers then there is no point to continue!
             break;
         end
-    else if(~isempty(ind) && numel(ind) == numInliers && mean(dist) < distMin )
+    else if(~isempty(ind) && numel(ind) == numInliers && mean(dist) < distMin ) % just in case we found a better group of inliers but with the same number of samples!
             distMin = mean(dist);
             Inliers    = ind;
         end
     end
 end
 
-% fix = [fix ones(size(fix, 1), 1)]*T_fix;
-% fix = fix(:, 1:3);
-% mov = [mov ones(size(mov, 1), 1)]*T_mov;
-% mov = mov(:, 1:3);
 
-samplesFix = fix(Inliers, :);
-samplesMov = mov(Inliers, :);
-[~, ~, Transformation] = procrustes(samplesFix, samplesMov, 'reflection', options.reflection);
-movRigid = bsxfun(@plus, Transformation.b *  mov * Transformation.T, Transformation.c(1, :));
-X = fix - movRigid;
-dist = sqrt(sum(X.^2, 2));
-ind = indices(dist<options.thr);
-if (numel(ind)>numInliers || (numel(ind) == numInliers && mean(dist) < distMin))
-    Inliers = ind;
-end
-
-
-samplesFix = fix(Inliers, :);
-samplesMov = mov(Inliers, :);
-[~, ~, T] = procrustes(samplesFix, samplesMov, 'scaling', false, 'reflection', options.reflection);
 if nargout==2
-    varargout{1} = Inliers;
+    samplesFix = fix(Inliers, :);
+    samplesMov = mov(Inliers, :);
+    [~, ~, T] = procrustes(samplesFix, samplesMov, 'scaling', false, 'reflection', options.reflection);
+    varargout{1} = T;
 end
-
-
-
-
-% figure
-% for ii = 1:size(imageEdge, 3)
-%     title(num2str(ii))
-%     imshow(imageEdge(:, :, ii));
-%     pause(0.1)
-% end
